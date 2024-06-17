84fcc3e6670169ec67b8424a21003432a7098eb8
vadniks <whatever@fake.some>
2024-06-17 17:30:49 +0300

    Structure & logic

--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -1,6 +1,7 @@
 
 #pragma once
 
+#include "defs.hpp"
 #include "Texture.hpp"
 #include <QOpenGLFunctions_3_3_Core>
 #include <glm/glm.hpp>
@@ -11,6 +12,10 @@ private:
 public:
     explicit Renderer(QOpenGLFunctions_3_3_Core& gl);
     ~Renderer();
+
+    DISABLE_COPY(Renderer)
+    DISABLE_MOVE(Renderer)
+
     void drawPoint(const glm::vec2& position, float pointSize, const glm::vec4& color);
     void drawLine(const glm::vec2& positionStart, const glm::vec2& positionEnd, float lineWidth, const glm::vec4& color);
     void drawRectangle(const glm::vec2& position, const glm::vec2& size, float lineWidth, const glm::vec4& color, bool filled);
--- src/Texture.hpp
+++ src/Texture.hpp
@@ -1,6 +1,21 @@
 
 #pragma once
 
-class Texture {
+#include "defs.hpp"
+#include <QOpenGLFunctions_3_3_Core>
 
+class Texture {
+private:
+    QOpenGLFunctions_3_3_Core& mGl;
+    unsigned mId;
+    int mWidth, mHeight;
+public:
+    Texture(QOpenGLFunctions_3_3_Core& gl, int width, int height, const QByteArray& data);
+    ~Texture();
+
+    DISABLE_COPY(Texture)
+    DISABLE_MOVE(Texture)
+
+    void bind();
+    QSize size();
 };
