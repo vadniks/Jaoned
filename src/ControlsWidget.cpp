5d582e3ef36340371516f457db0cdf0516e3329d
vadniks <whatever@fake.some>
2024-07-28 12:22:09 +0300

    Structure & logic

--- src/ControlsWidget.cpp
+++ src/ControlsWidget.cpp
@@ -179,9 +179,7 @@ void ControlsWidget::imageSelected(const QString& path) {
 
     modeSelected(Mode::IMAGE);
 
-    auto converted = image.toImage().convertToFormat(QImage::Format::Format_RGBA8888);
-    qDebug() << "image size " << image.width() * image.height() << ' ' << converted.sizeInBytes();
-    mBoardWidget->setCurrentTexture(glm::vec2(image.width(), image.height()), converted.constBits());
+    mBoardWidget->setCurrentTexture(glm::vec2(image.width(), image.height()), image.toImage().convertToFormat(QImage::Format::Format_RGBA8888).constBits());
 
     emit updated();
 }
